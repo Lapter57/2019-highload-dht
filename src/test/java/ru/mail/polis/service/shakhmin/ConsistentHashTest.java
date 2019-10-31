2cf9f21e56fa392b72e06b7e850eae42422c0c2b
Egor Shakhmin <lapter57@gmail.com>
2019-10-31 09:37:06 +0300

    added distribution test

--- src/test/java/ru/mail/polis/service/shakhmin/ConsistentHashTest.java
+++ src/test/java/ru/mail/polis/service/shakhmin/ConsistentHashTest.java
@@ -4,8 +4,12 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import ru.mail.polis.TestBase;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.LinkedHashSet;
+import java.util.Map;
 import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -13,34 +17,40 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class ConsistentHashTest extends TestBase {
+    private static final int KEYS_COUNT = 1_000_000;
+
     private int port0;
     private int port1;
+    private int port2;
     private Set<String> nodes;
     private ConsistentHash topology0;
     private ConsistentHash topology1;
-
-    private static final int KEYS_COUNT = 10_000_000;
+    private ConsistentHash topology2;
 
     @BeforeEach
     void beforeEach() {
         port0 = randomPort();
         port1 = randomPort();
+        port2 = randomPort();
 
         final var node0 = endpoint(port0);
         final var node1 = endpoint(port1);
-
-        nodes = new LinkedHashSet<>(Arrays.asList(node0, node1));
+        final var node2 = endpoint(port2);
+        
+        nodes = new LinkedHashSet<>(Arrays.asList(node0, node1, node2));
         topology0 = new ConsistentHashImpl(nodes, node0);
         topology1 = new ConsistentHashImpl(nodes, node1);
+        topology2 = new ConsistentHashImpl(nodes, node2);
     }
 
     @Test
     void consistentData() {
         for (int i = 0; i < KEYS_COUNT; i++) {
             final var key = randomKeyBuffer();
-            final var nodeFromTop0 = topology0.primaryFor(key.duplicate());
-            final var nodeFromTop1 = topology1.primaryFor(key.duplicate());
-            assertEquals(nodeFromTop0, nodeFromTop1);
+            final var primary0 = topology0.primaryFor(key.duplicate());
+            final var primary1 = topology1.primaryFor(key.duplicate());
+            final var primary2 = topology2.primaryFor(key.duplicate());
+            assertEquals(primary0, primary1, primary2);
         }
     }
 
@@ -69,5 +79,17 @@ class ConsistentHashTest extends TestBase {
 
         assertFalse(topology0.removeNode(removedNode));
         assertEquals(nodes, topology0.all());
+    }
+
+    @Test
+    void checkDistribution() {
+        final var nodesToNumKeys = new HashMap<String, Integer>();
+        for (long i = 0; i < KEYS_COUNT; i++) {
+            final var primary = topology0.primaryFor(randomKeyBuffer());
+            nodesToNumKeys.compute(primary, (n, c) -> c == null ? 1 : c + 1);
+        }
+        for (final var entry : nodesToNumKeys.entrySet()) {
+            assertTrue(entry.getValue() > KEYS_COUNT / (nodes.size() + 1) );
+        }
     }
 }
